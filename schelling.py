3b5e14605f1e46467502391e9d67ff3bf97363e1
Yichen Mo <amomorning@gmail.com>
2023-06-09 15:55:15 +0800

    Fix bug

--- schelling.py
+++ schelling.py
@@ -1,20 +1,20 @@
 import taichi as ti
-ti.init(ti.gpu)
+ti.init(ti.cpu, cpu_max_num_threads=1)
 
-cell_size = 8
-res = 100
+cell_size = 2
+res = 400
 pixels = ti.Vector.field(3, float, shape=(res*cell_size, res*cell_size))
 cell = ti.field(int, shape=(res, res))
 gui = ti.GUI('Schelling', res*cell_size)
 
 
 
-prob = [0, 0.1, 0.55]
+prob = [0, 0.08, 0.54]
 color = [ti.Vector([1, 1, 1]), ti.Vector([0.42, 0.54, 0.74]), ti.Vector([0.95, 0.81, 0.64])]
 
 num = int(res*res*prob[1])
 indices = ti.Vector.field(2, int, shape=(num, ))
-
+print(num)
 
 @ti.kernel
 def init() -> int:
@@ -30,20 +30,19 @@ def init() -> int:
 
         
 @ti.kernel
-def run():
-    # ti.loop_config(serialize=True)
+def run(p: float):
     for i, j in cell:
         if cell[i, j] == 0: continue
         same, tot = 0, 0
-        for u in range(-2, 3):        
-            for v in range(-2, 3):
+        for u in range(-1, 2):        
+            for v in range(-1, 2):
                 i_, j_ = i+u, j+v
                 if i_ >= 0 and j_ >= 0 and i_ < res and j_ < res:
-                    if cell[i_, j_] != 0:
-                        tot += 1
-                    if cell[i_, j_] != cell[i, j]:
+                    # if cell[i_, j_] != 0:
+                    tot += 1
+                    if cell[i_, j_] == cell[i, j]:
                         same += 1
-        if ti.cast(same-1, float)/(tot-1) < 0.5:
+        if ti.cast(same-1, float)/(tot-1) < p:
             idx = ti.random(int) % num
             i_, j_ = indices[idx]
             indices[idx] = ti.Vector([i, j])
@@ -63,6 +62,7 @@ def run():
 pause = False
 t = 0
 num = init()
+print(num)
 while gui.running:
     for e in gui.get_events(gui.PRESS, gui.MOTION):
         if e.key == gui.ESCAPE:
@@ -73,7 +73,7 @@ while gui.running:
             num = init()
 
     if not pause:
-        run()
+        run(0.5)
 
     gui.set_image(pixels)
     gui.show()
